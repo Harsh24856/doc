ea195f5a0caf9e1e6e3c641e6b5918a64e9d383a
Vedant1816 <angravedant@gmail.com>
2025-12-29 18:36:58 +0530

    Fixed CORS

--- back/server/server.js
+++ back/server/server.js
@@ -44,6 +44,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
     ? [
+        process.env.FRONTEND_URL,
         "http://localhost:5173",
       "http://localhost:5174",
       "http://127.0.0.1:5173",
@@ -113,6 +114,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === 'production'
       ? [
+      process.env.FRONTEND_URL,
       "http://localhost:5173",
       "http://localhost:5174",
           "http://127.0.0.1:5173",
